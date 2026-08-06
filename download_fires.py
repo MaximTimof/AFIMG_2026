d5b13421cd6991c6735127cb00b2bcc3f0d857f7
Максим <117020651+MaximTimof@users.noreply.github.com>
2026-08-06 19:26:15 +0900

    Update download_fires.py

--- download_fires.py
+++ download_fires.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 # URL страницы со списком отчетов
-# меняю даты вручную чтобы не упало август, потом июль, потом июнь
-TARGET_URL = "http://planet.iitp.ru/index.php?page_type=oper_prod&page=fire_report&start_date=2026-06-01&end_date=2026-06-30&region=all"
+# меняю даты вручную чтобы не упало август, потом июль, потом июнь, потом от января по май
+TARGET_URL = "http://planet.iitp.ru/index.php?page_type=oper_prod&page=fire_report&start_date=2026-01-01&end_date=2026-05-31&region=all"
 # Папка, куда будут сохраняться файлы
 OUTPUT_DIR = "downloaded_pdfs"
 
